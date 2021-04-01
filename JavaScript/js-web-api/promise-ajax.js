6118ea36619983cd24421ec78777f8507588f867
hzy <1257664828@qq.com>
2021-04-01 16:42:58 +0800

    跨域

--- JavaScript/js-web-api/promise-ajax.js
+++ JavaScript/js-web-api/promise-ajax.js
@@ -1,24 +1,22 @@
-function ajax(url){
-    const p = new Promise((resolve,reject) => {
+// Promise封装一个ajax
+function ajax(url) {
+    return new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();
-        xhr.open('GET',url,true);
         xhr.onreadystatechange = function () {
-            if(xhr.readyState === 4) {
-                if(xhr.status === 200) {
-                    resolve(
-                        JSON.parse(xhr.responseText)
-                    )
-                } else if (xhr.status === 404 || xhr.status === 500){
-                    reject(new Error('404 not found'))
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    resolve(JSON.parse(xhr.resopnseText))
                 }
+            } else if (xhr.status === 404 || xhr.status === 500) {
+                reject(new Error('404 not found'))
             }
         }
+        xhr.open('GET', 'url', true);
         xhr.send(null);
     })
-    return p;
 }
 
-const  url = '/data/test.json'
+const url = '/data/test.json'
 ajax(url)
-.then(res => console.log(res))
-.catch(err => console.error(err))
+    .then(res => console.log(res))
+    .catch(err => console.error(err))
