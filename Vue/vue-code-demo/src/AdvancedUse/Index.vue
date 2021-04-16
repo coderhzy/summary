b7824ff8bb90fe96b24a1c012d92bf1c2849c6b1
hzy <1257664828@qq.com>
2021-04-16 10:26:29 +0800

    具名插槽

--- Vue/vue-code-demo/src/AdvancedUse/Index.vue
+++ Vue/vue-code-demo/src/AdvancedUse/Index.vue
@@ -8,10 +8,18 @@
         <!-- <SlotDemo>
             {{name}}
         </SlotDemo> -->
-        <ScopedSlotDemo :name="name">
-            <!-- {{name}} -->
-        </ScopedSlotDemo>
-
+        <!-- <ScopedSlotDemo :name="name">
+            {{name}}
+        </ScopedSlotDemo> -->
+        <NamedSlot>
+            <template v-slot:header>
+                <h1>将插入 header slot中</h1>
+            </template>
+            <p>将插入到main slot中，即未命名slot</p>
+            <template v-slot:footer> 
+                <p>将插入到footer slot中</p>
+            </template>
+        </NamedSlot>
     </div>
 
 </template>
@@ -20,14 +28,16 @@
 // import CustomVModel from './CustomVModel'
 // import NextTick from './NextTick'
 // import SlotDemo from './SlotDemo'
-import ScopedSlotDemo from "./ScopedSlotDemo"
+// import ScopedSlotDemo from "./ScopedSlotDemo"
+import NamedSlot from './NamedSlot'
 
 export default {
     components: {
         // CustomVModel
         // NextTick
         // SlotDemo
-        ScopedSlotDemo
+        // ScopedSlotDemo
+        NamedSlot
     },
     data() {
         return {
---
+++ Vue/vue-code-demo/src/AdvancedUse/NamedSlot.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <header>
+            <slot name="header"></slot>
+        </header>
+
+        <main>
+            <slot></slot>
+        </main>
+
+        <footer>
+           <slot name="footer"></slot>
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        type: String,
+        defult() {
+            return ''
+        }
+    },
+    data() {
+        return {
+            name: 'hzy'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
